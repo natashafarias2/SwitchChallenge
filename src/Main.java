4fbfc3a0501d5278eb947dcf4794256f021d17a9
natashafarias2 <118220162+natashafarias2@users.noreply.github.com>
2025-11-03 00:33:52 -0300

    Add switch statement for character evaluation

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+
+        char charValue = 'B';
+        switch (charValue) {
+            case 'A':
+                System.out.println("A is able");
+                break;
+            case 'B':
+                System.out.println("B is baker");
+                break;
+            case 'C':
+                System.out.println("C is charlie");
+                break;
+            case 'D':
+                System.out.println("D is dog");
+                break;
+            case 'E':
+                System.out.println("E is easy");
+                break;
+            default:
+                System.out.println("Letter " + charValue + " was not found in the switch");
+                break;
         }
+
+
+
     }
 }
